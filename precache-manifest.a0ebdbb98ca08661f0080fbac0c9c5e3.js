8d73ef14e252569349ffb2b955f21434334e866f
shubhamneman <65542711+shubhamneman@users.noreply.github.com>
2024-01-14 18:01:26 +0530

    Updates

---
+++ precache-manifest.a0ebdbb98ca08661f0080fbac0c9c5e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595698521f1d73087a6551a1359e9780",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "3cd4919ee97c5bb18480",
+    "url": "/TextUtils/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/TextUtils/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/TextUtils/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "3cd4919ee97c5bb18480",
+    "url": "/TextUtils/static/js/main.c8aae339.chunk.js"
+  },
+  {
+    "revision": "addae886a4d582bb8f4d",
+    "url": "/TextUtils/static/js/runtime~main.b9ff5895.js"
+  }
+]);
